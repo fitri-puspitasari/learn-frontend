0f636315bc6a88c355e6b22e91dc75905dabc9ab
Fitri Puspitasari <fitripuspitasari132@gmail.com>
2024-11-10 00:58:30 +0700

    3) Menambah empty-note-message pada note-list

--- 3-Personal_Notes_App/src/components/NotesContent/NotesListArea.jsx
+++ 3-Personal_Notes_App/src/components/NotesContent/NotesListArea.jsx
@@ -7,19 +7,24 @@ function NotesListArea({ listClass, title, dataNote, onChangeStatusArchived, onD
         <div className={listClass}>
             <h3>{title}</h3>
             <div className="notes-list">
-                {dataNote.map((data) => (
-                    <div className="note-item" key={data.id}>
-                        <div className="note-item__texts">
-                            <h5 className="note-item__title">{data.title}</h5>
-                            <p className="note-item__date">{showFormattedDate(data.createdAt)}</p>
-                            <p className="note-item__body">{data.body}</p>
+                {dataNote.length == 0 ?
+                    <p className="notes-list__empty-note-message">Tidak ada catatan</p>
+                    :
+                    dataNote.map((data) => (
+                        <div className="note-item" key={data.id}>
+                            <div className="note-item__texts">
+                                <h5 className="note-item__title">{data.title}</h5>
+                                <p className="note-item__date">{showFormattedDate(data.createdAt)}</p>
+                                <p className="note-item__body">{data.body}</p>
+                            </div>
+                            <div className="note-item__buttons">
+                                <button className="primary-button" onClick={() => onChangeStatusArchived(data.id)}>{title == "Catatan Aktif" ? "Arsipkan" : "Pindahkan"}</button>
+                                <button className="danger-button" onClick={() => onDelete(data.id)}>Hapus</button>
+                            </div>
                         </div>
-                        <div className="note-item__buttons">
-                            <button className="primary-button" onClick={() => onChangeStatusArchived(data.id)}>{title == "Catatan Aktif" ? "Arsipkan" : "Pindahkan"}</button>
-                            <button className="danger-button" onClick={() => onDelete(data.id)}>Hapus</button>
-                        </div>
-                    </div>
-                ))}
+                    ))
+                }
+                
             </div>
         </div>
     )
